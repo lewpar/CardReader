beac8e7ea8c60ff39a9ac3b41352c4096cc8b37a
lewpar <lewis.o.parmenter@gmail.com>
2024-01-17 15:02:17 +1100

    Initial project commit.

---
+++ Arduino/CardReader.ino
@@ -0,0 +1,82 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN 9
+#define SS_PIN 10
+
+enum OpCodes : uint8_t
+{
+  OPCODE_RFID_READ = 1,
+  OPCODE_RFID_LOCK_STATE = 2
+};
+
+bool locked = false;
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+	Serial.begin(9600);
+	SPI.begin();
+	mfrc522.PCD_Init();
+}
+
+void loop() {
+  // If the card reader is locked waiting for response.
+  if(isLocked())
+  {
+    // Check the serial buffer for unlock signal.
+    checkForUnlock();
+    return;
+  }
+
+	// Check if there is a card present to read.
+	if (!mfrc522.PICC_IsNewCardPresent()) {
+		return;
+	}
+
+	// Check to see if the card if readable.
+	if (!mfrc522.PICC_ReadCardSerial()) {
+		return;
+	}
+
+  Serial.write(OPCODE_RFID_READ); // OpCode
+  Serial.write(mfrc522.uid.size); // UidLen
+  for (byte i = 0; i < mfrc522.uid.size; i++) // Uid
+  {
+    Serial.write(mfrc522.uid.uidByte[i]);
+  }
+
+  // Lock the card reader.
+  setLock(true);
+}
+
+bool isLocked()
+{
+  return locked;
+}
+
+void setLock(bool state)
+{
+  locked = state;
+
+  // Send lock state through Serial.
+  Serial.write(OPCODE_RFID_LOCK_STATE); // OpCode
+  Serial.write(locked); // Lock State
+}
+
+void checkForUnlock()
+{
+  if(!Serial.available())
+  {
+    return;
+  }
+
+  // Read 1 byte from serial buffer.
+  int data = Serial.read();
+
+  // Treat data as a bool, if the value is true then unlock the reader.
+  if(data)
+  {
+    setLock(false);
+  }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.IO.Ports;
+
+namespace SerialTest
+{
+    enum ArduinoOpCodes : byte
+    {
+        RFID_READ = 0x01,
+        RFID_LOCK_STATE = 0x02
+    }
+
+    internal class Program
+    {
+        private static Dictionary<byte, Func<SerialPort, Task>> opCodes = new()
+        {
+            { (byte)ArduinoOpCodes.RFID_READ, HandleRfidReadOpCodeAsync },
+            { (byte)ArduinoOpCodes.RFID_LOCK_STATE, HandleRfidLockStateOpCodeAsync }
+        };
+
+        static async Task Main(string[] args)
+        {
+            var ports = SerialPort.GetPortNames();
+            int? selectedPort = null;
+
+            while (selectedPort is null)
+            {
+                Console.WriteLine("Select COM port to continue:");
+                for (int i = 0; i < ports.Length; i++)
+                {
+                    Console.WriteLine($"{i}: {ports[i]}");
+                }
+
+                var option = Console.ReadLine();
+
+                if (!int.TryParse(option, out int result))
+                {
+                    Console.WriteLine("Invalid option.");
+                    Console.WriteLine();
+                    continue;
+                }
+
+                selectedPort = result;
+            }
+
+            Console.WriteLine($"Port '{ports[selectedPort.Value]}' selected.");
+
+            var port = new SerialPort()
+            {
+                PortName = ports[selectedPort.Value],
+                BaudRate = 9600,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 8,
+                Handshake = Handshake.None,
+                RtsEnable = true,
+            };
+
+            port.Open();
+
+            Console.WriteLine("Waiting for NFC Tag..");
+
+            port.DataReceived += Port_DataReceived;
+
+            await Task.Delay(-1);
+        }
+
+        private static void Port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            var port = sender as SerialPort;
+            if (port is null)
+            {
+                return;
+            }
+
+            if (!port.IsOpen)
+            {
+                Console.WriteLine("Serial Port not open.");
+                return;
+            }
+
+            Console.WriteLine("Received data from Arduino..");
+
+            _ = HandleRecvAsync(port);
+        }
+
+        static async Task HandleRecvAsync(SerialPort port)
+        {
+            byte opCode = (byte)port.ReadByte();
+
+            Console.WriteLine($"Received OpCode: {opCode}");
+
+            if (!opCodes.ContainsKey(opCode))
+            {
+                Console.WriteLine($"Received invalid OpCode: {opCode}");
+                return;
+            }
+
+            await opCodes[opCode](port);
+        }
+
+        static async Task HandleRfidReadOpCodeAsync(SerialPort port)
+        {
+            Console.WriteLine("Handling TestMessage OpCode");
+
+            int len = port.ReadByte();
+
+            Console.WriteLine($"Got length: {len}");
+
+            byte[] key = new byte[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                key[i] = (byte)port.ReadByte();
+            }
+
+            Console.Write("Got NFC Tag UID: ");
+            foreach (byte b in key)
+            {
+                Console.Write($"{b:X} ");
+            }
+
+            Console.WriteLine();
+
+            // Wait 2 seconds before unlocking card reader.
+            await Task.Delay(2000);
+
+            // Sending any data (more than 0) to the card reader while it's locked will unlock it.
+            Console.WriteLine("Sending unlock signal to Arduino..");
+            port.Write(new byte[] { 0x01 }, 0, sizeof(byte));
+        }
+
+        static Task HandleRfidLockStateOpCodeAsync(SerialPort port)
+        {
+            Console.WriteLine("Handing LockState OpCode");
+
+            bool lockState = port.ReadByte() > 0 ? true : false;
+            Console.WriteLine($"Arduino Reader is: {(lockState ? "Locked" : "Unlocked")}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
